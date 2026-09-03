f5535d655af76b89713bd7aa044ebf126c08f1f1
Den-v-code <skaner45@gmail.com>
2026-09-03 17:28:30 +0500

    fix(proofs): bind R9 intermediate objects

--- src/core/intrinsic_mode_manifest.py
+++ src/core/intrinsic_mode_manifest.py
@@ -2,6 +2,36 @@
 from types import MappingProxyType
 
 TCB_SCHEMA = "veyra-intrinsic-mode-tcb-v1"
+EXPECTED_LEAN_OBJECTS = MappingProxyType({
+    "lean_arithmetic": (
+        "VeyraNativeArithmetic.olean", 176856,
+        "2ee38abbf195fcc2f0837366af98ca3047b1bc682691c049c3c9fe3eb44b1945",
+    ),
+    "lean_semantics": (
+        "VeyraNativeSemantics.olean", 767352,
+        "e3ff6f807e2570e8b637e9fab76c786023339c6f24c6acf5866ba55b4337e505",
+    ),
+    "lean_intrinsic_runtime": (
+        "VeyraIntrinsicRuntime.olean", 81928,
+        "49230a22143b1eec485f2f97e788608fdc833a963493ab369e62fdad359b30cf",
+    ),
+    "lean_kernel": (
+        "VeyraProofKernel.olean", 2353728,
+        "0b42aca8cab44c3bd40355379f95ff3845b3cc24ce9d24984e1be2721603c4c1",
+    ),
+    "lean_soundness": (
+        "VeyraProofSoundness.olean", 611240,
+        "935ce61c4375afcb8a5c3275b1f17f34d5e83c24f6fc54b8324f29941dbb6e1a",
+    ),
+    "lean_r7_export": (
+        "VeyraProofResonance.olean", 36608,
+        "ab0f67f5cbbbc8e360e84d4f26ad4119205b885da137d3179867530482c28cf1",
+    ),
+    "lean_transport": (
+        "VeyraRecurrenceModeBridge.olean", 261136,
+        "b2b616349e095f2b436fbf9fbaee561fb39f618ff4e51693f23fd1093b2e201f",
+    ),
+})
 EXPECTED_R9_TCB_DIGESTS = MappingProxyType({
     "python_transport": "b037c828eee79fe1c482df180cd1f5c4538a63eae5bf212511ba804322a19184",
     "python_laws": "e82e1c6a65c9752d1052390c9d5b74ab23d0d9b4ef29c210d10d824cbdfbd97f",
